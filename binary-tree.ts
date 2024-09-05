db5f4d92dd7bda322102ad5fe0cb4cf362316830
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2024-09-05 21:26:13 +0200

    implement binary tree

--- binary-tree.ts
+++ binary-tree.ts
@@ -1,98 +1,49 @@
-class RootNode<T> {
-  data: T;
-  left: RootNode<T> | null;
-  right: RootNode<T> | null;
+class BinaryTree{
+  root: BinaryTreeNode;
 
-  constructor(data: T) {
+  constructor(rootNode: BinaryTreeNode){
+    this.root = rootNode;
+  }
+
+  breadthFirstTraversal(){
+    const queue: BinaryTreeNode[] = [this.root];
+
+    while(queue.length > 0){
+      const node = queue.shift()!;
+
+      console.log(node.data);
+
+      if(node.left){
+        queue.push(node.left)
+      }
+
+      if(node.right){
+        queue.push(node.right)
+      }
+    }
+  }
+}
+
+class BinaryTreeNode {
+  data: any;
+  left: BinaryTreeNode | null = null;
+  right: BinaryTreeNode | null = null;
+  
+  constructor(data: any){
     this.data = data;
-    this.left = null;
-    this.right = null;
   }
 }
 
-class BinaryTree<T> {
-  root: RootNode<T> | null;
+const node1 = new BinaryTreeNode(10)
+const node2 = new BinaryTreeNode(20)
+const node3 = new BinaryTreeNode(30)
+const node4 = new BinaryTreeNode(40)
+const node5 = new BinaryTreeNode(50)
 
-  constructor() {
-    this.root = null;
-  }
+node1.left = node2;
+node1.right = node3;
+node2.right = node4;
+node3.left = node5;
 
-  insert(data: T) {
-    const newNode = new RootNode(data);
-
-    if (!this.root) {
-      this.root = newNode;
-    }
-
-    this.insertInSubtree(this.root, newNode);
-
-    return this;
-  }
-
-  private insertInSubtree<T>(node: RootNode<T>, newNode: RootNode<T>) {
-    if (newNode.data > node.data) {
-      if (node.right) {
-        this.insertInSubtree(node.right, newNode);
-      } else {
-        node.right = newNode;
-      }
-    } else if (newNode.data < node.data) {
-      if (node.left) {
-        this.insertInSubtree(node.left, newNode);
-      } else {
-        node.left = newNode;
-      }
-    } else {
-      return;
-    }
-  }
-
-  search(data: any) {
-    if (!this.root) {
-      return null;
-    }
-
-    return this.searchSubtree<any>(this.root, data);
-  }
-
-  private searchSubtree<T>(node: RootNode<T>, data: T): any {
-    if (node.data === data) {
-      return node;
-    } else if (data > node.data) {
-      if (node.right) {
-        return this.searchSubtree(node.right, data);
-      } else {
-        return null;
-      }
-    } else if (data < node.data) {
-      if (node.left) {
-        return this.searchSubtree(node.left, data);
-      } else {
-        return null;
-      }
-    }
-  }
-}
-
-const binaryTree = new BinaryTree();
-
-binaryTree
-  .insert(10)
-  .insert(100)
-  .insert(-100)
-  .insert(2)
-  .insert(50)
-  .insert(100)
-  .insert(50);
-
-console.log(binaryTree.root);
-
-console.log(binaryTree.root?.left);
-
-console.log(binaryTree.root?.right);
-
-console.log(binaryTree.search(100));
-
-console.log(binaryTree.search(2));
-
-console.log(binaryTree.search(2000));
+const binaryTree = new BinaryTree(node1);
+binaryTree.breadthFirstTraversal()
