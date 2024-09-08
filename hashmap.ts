ee8cf9154b7833c7bd61b509c9d141f9231826bd
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2024-09-08 13:46:30 +0200

    feat(hashmap): :sparkles: hashmap practice

--- hashmap.ts
+++ hashmap.ts
@@ -1,41 +1,83 @@
-class HashMap {
-  capacity = 10;
-  buckets: (LinkedList | undefined)[] = Array(this.capacity).fill(undefined);
+class HashMap{
+  private balanceFactor = Infinity;
+  private numberOfValues = 0;
+  private numberOfBuckets = 4;
+  private buckets = Array.from({length: this.numberOfBuckets}, () => new LinkedList())
 
-  get(key: string) {
-    const hash = this.getHash(key);
+  set(key: string, value: any){
+    const bucketIndex = this.hash(key);
 
-    if (!this.buckets[hash]) {
-      return;
-    }
+    const existingKeyNode = this.buckets[bucketIndex].find(key);
 
-    return this.buckets[hash]?.find(key)?.data?.data;
-  }
+    if(existingKeyNode){
+      this.buckets[bucketIndex].update(existingKeyNode, key, value);
+    }else{
+      this.buckets[bucketIndex] = this.buckets[bucketIndex].add(key, value);
 
-  set(key: string, value: any) {
-    const hash = this.getHash(key);
+      this.numberOfValues++;
+  
+      this.balanceFactor = this.numberOfValues / this.numberOfBuckets;
 
-    if (!this.buckets[hash]) {
-      this.buckets[hash] = new LinkedList().add(key, value);
-    } else {
-      this.buckets[hash]?.add(key, value);
+      if(this.balanceFactor > 0.7){
+        this.rehash();
+      }
     }
 
     return this;
   }
 
-  delete(key: string) {
-    const hash = this.getHash(key);
+  get(key: string){
+    const hashValue = this.hash(key);
 
-    if (!this.buckets[hash]) {
-      return;
-    }
-
-    return this.buckets[hash]?.remove(key);
+    return this.buckets[hashValue].find(key)?.data.data;
   }
 
-  private getHash(key: string) {
-    return key.length % this.capacity;
+  delete(key: string){
+    const hashValue = this.hash(key);
+
+    this.balanceFactor = this.numberOfValues / this.numberOfBuckets;
+
+    const removedNode = this.buckets[hashValue].remove(key);
+
+    if(removedNode){
+      this.numberOfValues--;
+    }
+
+    return removedNode?.data.data;
+  }
+
+  private rehash(){
+    const bucketItems: LinkedListNode[] = [];
+
+    this.buckets.forEach((linkedList, index) => {
+      linkedList.traverse((linkedListNode) => {
+        bucketItems.push(linkedListNode)
+      })
+    })
+
+    this.numberOfBuckets = this.numberOfBuckets * 2;
+
+    this.numberOfValues = 0;
+
+    this.balanceFactor = Infinity;
+
+    this.buckets = Array.from({length: this.numberOfBuckets}, () => new LinkedList())
+
+    bucketItems.forEach(bucketItem => {
+      this.set(bucketItem.data.key, bucketItem.data.data)
+    })
+
+    return;
+  }
+
+  private hash(value: string){
+    let hashValue = 0;
+
+    for(let i = 0; i < value.length; i++){
+      hashValue += value.charCodeAt(i);
+    }
+
+    return hashValue % this.numberOfBuckets;
   }
 }
 
@@ -111,6 +153,21 @@ class LinkedList {
 
     return foundNode;
   }
+
+  update(node: LinkedListNode, key: any, value: any){
+    node.data.key = key;
+    node.data.data = value;
+  }
+
+  traverse(callback: (node: LinkedListNode) => void){
+    let currentNode = this.head;
+
+    while(currentNode){
+      callback(currentNode);
+
+      currentNode = currentNode.next;
+    }
+  }
 }
 
 const hashMap = new HashMap();
@@ -118,9 +175,16 @@ const hashMap = new HashMap();
 hashMap.set("a", 123);
 hashMap.set("b", 456);
 hashMap.set("ab", 789);
+hashMap.set("abc", 100);
+hashMap.set("abcd", 1000);
+console.log(hashMap);
+hashMap.set("abcde", 10000);
+hashMap.set("abcde", 100000);
 console.log(hashMap);
 console.log(hashMap.get("a"));
 console.log(hashMap.get("b"));
 console.log(hashMap.get("ab"));
+console.log(hashMap.get("abc"));
+console.log(hashMap.get("abcde"))
 hashMap.delete("a");
 console.log(hashMap.get("a"));
