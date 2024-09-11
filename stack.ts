12e8a27ff6a725bf5ec25c5d8fadff61416cc07b
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2024-09-11 18:49:45 +0200

    feat(stack): :sparkles: stack practice

--- stack.ts
+++ stack.ts
@@ -1,21 +1,74 @@
 class Stack {
-  private list: any[] = [];
-  constructor() {}
+  private list: LinkedList;
 
-  push(data: any) {
-    return this.list.push(data);
+  constructor() {
+    this.list = new LinkedList();
+  }
+
+  push(value: any) {
+    return this.list.addToHead(value);
   }
 
   pop() {
-    return this.list.pop();
+    return this.list.removeFromHead();
   }
 
   peek() {
-    return this.list[this.list.length - 1];
+    return this.list.getHead();
+  }
+}
+
+class LinkedList {
+  head: LinkedListNode | null;
+
+  constructor() {
+    this.head = null;
   }
 
-  isEmpty() {
-    return this.list.length === 0;
+  addToHead(data: any) {
+    const node = new LinkedListNode(data);
+
+    const head = this.head;
+
+    if (!this.head) {
+      this.head = node;
+    }
+
+    node.next = head;
+
+    this.head = node;
+
+    return this;
+  }
+
+  removeFromHead() {
+    const head = this.head;
+
+    if (!head) {
+      return head;
+    }
+
+    this.head = head.next;
+
+    return head.data;
+  }
+
+  getHead() {
+    if (this.head) {
+      return this.head.data;
+    }
+
+    return this.head;
+  }
+}
+
+class LinkedListNode {
+  data: any;
+  next: LinkedListNode | null;
+
+  constructor(data: any) {
+    this.data = data;
+    this.next = null;
   }
 }
 
