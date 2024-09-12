c3b655d4282618930dda28d37422ebb55562c2ff
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2024-09-12 20:15:57 +0200

    feat(singly-linked-list): :sparkles: singly linked list practice

--- singly-linked-list.ts
+++ singly-linked-list.ts
@@ -1,155 +1,107 @@
-class ListNode<T> {
-  data: T | null;
-  next: ListNode<T> | null;
+class SinglyLinkedList {
+  head: SinglyLinkedListNode | null;
+  constructor() {
+    this.head = null;
+  }
 
-  constructor(data: T) {
-    this.data = data;
-    this.next = null;
+  insertAtStart(value: number) {
+    const node = new SinglyLinkedListNode(value);
+
+    node.next = this.head;
+
+    this.head = node;
+
+    return this;
+  }
+
+  insertAtEnd(value: number) {
+    let lastNode = this.head;
+
+    this.traverse((currentNode) => {
+      if (currentNode.next === null) {
+        lastNode = currentNode;
+      }
+    });
+
+    const node = new SinglyLinkedListNode(value);
+
+    if (lastNode === null) {
+      this.head = node;
+    } else {
+      lastNode.next = node;
+    }
+
+    return this;
+  }
+
+  removeFromStart() {
+    let nodeToBeRemoved = this.head;
+
+    if (nodeToBeRemoved !== null) {
+      this.head = nodeToBeRemoved.next;
+    }
+
+    return nodeToBeRemoved;
+  }
+
+  removeFromEnd() {
+    let nodeBeforeNodeToBeRemoved: SinglyLinkedListNode | null = null;
+    let nodeToBeRemoved = this.head;
+
+    this.traverse((node) => {
+      if (node.next === null) {
+        nodeToBeRemoved = node;
+      } else {
+        nodeBeforeNodeToBeRemoved = node;
+      }
+    });
+
+    if (nodeToBeRemoved === null) {
+      return null;
+    }
+
+    if (nodeToBeRemoved === this.head) {
+      this.head = null;
+    } else {
+      (nodeBeforeNodeToBeRemoved as unknown as SinglyLinkedListNode).next =
+        null;
+    }
+
+    return nodeToBeRemoved;
+  }
+
+  traverse(callback: (node: SinglyLinkedListNode) => void) {
+    let currentNode = this.head;
+    while (currentNode) {
+      callback(currentNode);
+
+      currentNode = currentNode.next;
+    }
   }
 }
 
-class SinglyLinkedList<T> {
-  head: ListNode<T> | null = null;
-  tail: ListNode<T> | null = null;
+class SinglyLinkedListNode {
+  next: SinglyLinkedListNode | null;
+  value: number;
 
-  insertAtBeginning(data: T) {
-    const newNode = new ListNode(data);
-
-    if (this.head && this.tail) {
-      newNode.next = this.head;
-      this.head = newNode;
-    } else {
-      this.head = newNode;
-      this.tail = newNode;
-    }
-
-    return this;
-  }
-
-  insertAtEnd(data: T) {
-    const newNode = new ListNode(data);
-
-    if (this.head && this.tail) {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    } else {
-      this.head = newNode;
-      this.tail = newNode;
-    }
-
-    return this;
-  }
-
-  insertBeforeNode(node: ListNode<T>, data: T) {
-    let beforeNode: ListNode<T> | null = null;
-    let currentNode = this.head;
-    let foundNode: ListNode<T> | null = null;
-
-    while (foundNode === null && currentNode !== null) {
-      if (node === currentNode) {
-        foundNode = currentNode;
-        break;
-      }
-
-      beforeNode = currentNode;
-      currentNode = currentNode.next;
-    }
-
-    if (foundNode) {
-      const newNode = new ListNode(data);
-
-      newNode.next = foundNode;
-
-      if (beforeNode) {
-        beforeNode.next = newNode;
-      }
-
-      if (foundNode === this.head) {
-        this.head = newNode;
-      }
-    }
-
-    return this;
-  }
-
-  insertAfterNode(node: ListNode<T>, data: T) {
-    let foundNode: ListNode<T> | null = null;
-    let currentNode = this.head;
-
-    while (foundNode === null && currentNode !== null) {
-      if (node.data === currentNode.data) {
-        foundNode = currentNode;
-      }
-
-      currentNode = currentNode.next;
-    }
-
-    if (!foundNode) {
-      return null;
-    }
-
-    const newNode = new ListNode(data);
-
-    newNode.next = foundNode.next;
-    foundNode.next = newNode;
-
-    if (foundNode === this.tail) {
-      this.tail = newNode;
-    }
-
-    return this;
-  }
-
-  insertAtIndex(index: number, data: T) {
-    if (index < 0) {
-      return null;
-    }
-
-    let currentNode: ListNode<T> | null = this.head;
-    let prevNode: ListNode<T> | null = null;
-    let currentIndex = 0;
-    let visitedEachElement = false;
-
-    while (visitedEachElement === false) {
-      if (currentNode === null) {
-        visitedEachElement = true;
-        break;
-      }
-
-      if (currentIndex === index) {
-        visitedEachElement = true;
-        break;
-      }
-
-      currentIndex++;
-      prevNode = currentNode;
-      currentNode = currentNode.next;
-    }
-
-    const newNode = new ListNode(data);
-
-    if (prevNode) {
-      prevNode.next = newNode;
-    }
-
-    newNode.next = currentNode;
-
-    if (this.head === currentNode) {
-      this.head = newNode;
-    }
-
-    if (this.tail === currentNode) {
-      this.tail = newNode;
-    }
-
-    return this;
+  constructor(value: number) {
+    this.next = null;
+    this.value = value;
   }
 }
 
 const linkedList = new SinglyLinkedList();
 
+linkedList.insertAtStart(100);
+console.log(linkedList);
 linkedList.insertAtEnd(0);
 linkedList.insertAtEnd(1);
 linkedList.insertAtEnd(2);
-
 console.log(linkedList);
+linkedList.insertAtStart(10);
+console.log(linkedList);
+console.log(linkedList.removeFromEnd());
+console.log(linkedList.removeFromEnd());
+console.log(linkedList.removeFromEnd());
+console.log(linkedList.removeFromEnd());
+console.log(linkedList.removeFromEnd());
