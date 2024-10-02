958f8e9eb802a488a889b184ea2404171afb4557
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2024-10-02 16:18:35 +0200

    feat(rabin-karp): :sparkles: rabin karp practice

--- rabin-karp.ts
+++ rabin-karp.ts
@@ -1,36 +1,42 @@
 class RabinKarp {
   text: string;
-  substring: string;
+  pattern: string;
 
-  constructor(text: string, substring: string) {
+  constructor(text: string, pattern: string) {
     this.text = text;
-    this.substring = substring;
-  }
-
-  hash(value: string) {
-    let hash = 0;
-
-    for (const character of value) {
-      hash += character.charCodeAt(0);
-    }
-
-    return hash;
+    this.pattern = pattern;
   }
 
   find() {
-    for (let i = 0; i <= this.text.length - this.substring.length; i++) {
-      const substringHash = this.hash(this.substring);
-      const textWindowHash = this.hash(
-        this.text.slice(i, i + this.substring.length)
-      );
+    const hashedPattern = this.hash(this.pattern);
 
-      if (
-        substringHash === textWindowHash &&
-        this.substring === this.text.slice(i, i + this.substring.length)
-      ) {
-        return i;
+    let startIndex = 0;
+    let endIndex = startIndex + this.pattern.length;
+
+    while (this.text.length >= endIndex) {
+      const textSlice = this.text.slice(startIndex, endIndex);
+
+      const hashedTextSlice = this.hash(textSlice);
+
+      if (hashedPattern === hashedTextSlice) {
+        if (this.pattern === textSlice) {
+          return textSlice;
+        }
       }
+
+      startIndex += 1;
+      endIndex += 1;
     }
+  }
+
+  private hash(value: string) {
+    let hashedValue: number = 0;
+
+    for (let i = 0; i < value.length; i++) {
+      hashedValue += value.charCodeAt(i);
+    }
+
+    return hashedValue;
   }
 }
 
